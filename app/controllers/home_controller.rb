365a47021b466a630b487ea0a98872ee37f1cfd2
ai818 <aiyamamoto818@gmail.com>
2019-11-24 23:24:26 +0900

    1124

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+
+  def index
+    render html: "ここに紹介ページをかきます"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'reviews#index'
+  # devise_for :users
+  root 'home#index'
 end
